ef62e35c2176373565e235131b6c56a338fb1771
tentaShiratori <tenta.shiratori@gmail.com>
2024-10-04 04:01:09 +0900

    いっけええええ

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './components/App.tsx'
+import { createBrowserRouter, RouterProvider, useParams } from 'react-router-dom'
+import App from './components/App';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/blogs/:id",
+    Component: ()=>{
+      const params = useParams<{id:string}>();
+    return <div>blogs { params.id}</div>
+    },
+  }
+]);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+ <RouterProvider router={router} />
   </StrictMode>,
 )
